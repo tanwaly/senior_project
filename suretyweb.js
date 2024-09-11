1e1702b4276fc11c1c69b4b7d131354784100ef4
tanwaly <tanwalainiumsawan@gmail.com>
2024-09-11 14:36:54 +0700

    update

--- suretyweb.js
+++ suretyweb.js
@@ -34,29 +34,7 @@ const Idupload = multer({ storage: storage }).single('id_img');
 // app.get("/", (req, res) => {
 //     res.sendFile(path.join(__dirname, 'Project/customer/c-homepage.html'));
 // });
-//--------- login ---------
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const sql = "SELECT users_id, password, role FROM users WHERE email = ?";
-    con.query(sql, [email], (err, results) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send("Database server error");
-        }
-        if (results.length != 1) {
-            return res.status(400).send("Wrong email");
-        }
-        bcrypt.compare(password, results[0].password, (err, same) => {
-            if (err) {
-                res.status(503).send("Authentication server error");
-            } else if (same) {
-                res.send("Login complete");
-            } else {
-                res.status(400).send("Wrong password");
-            }
-        });
-    });
-});
+
 
 //--------- register ---------
 app.get('/register', (req, res) => {
@@ -81,7 +59,7 @@ app.post('/register', Idupload, async (req, res) => {
         const bcryptPass = await bcrypt.hash(req.body.password, 10);
 
         // Prepare the INSERT query
-        const sqlInsert = 'INSERT INTO users (first_name, last_name, email, password, phonenum, role, id_img, bank_ac_name, bank_ac_num, user_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        const sqlInsert = 'INSERT INTO users (first_name, last_name, email, password, phonenum, role, id_img, bank_ac_name, bank_ac_num, user_status,profile_img) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)';
         const insertParams = [
             req.body.first_name,
             req.body.last_name,
@@ -92,7 +70,8 @@ app.post('/register', Idupload, async (req, res) => {
             req.file ? req.file.filename : null,
             req.body.bank_ac_name || null,
             req.body.bank_ac_num || null,
-            1 // Default user status
+            1, // Default user status
+            'fprofile.jpg'
         ];
 
         con.query(sqlInsert, insertParams, (err, result) => {
@@ -100,17 +79,46 @@ app.post('/register', Idupload, async (req, res) => {
                 console.error(err);
                 return res.status(500).send("DB error");
             }
-            res.send('Registration complete');
+            res.send('login');
         });
 
     });
 });
 
+//--------- login ---------
 app.get('/login', function (_req, res) {
     res.sendFile(path.join(__dirname, 'Project/login.html'));
 });
 
-//--------- convert password ---------
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const sql = "SELECT users_id, password, role FROM users WHERE email = ?";
+    con.query(sql, [email], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Database server error");
+        }
+        if (results.length !== 1) {
+            return res.status(400).send("Wrong email");
+        }
+        bcrypt.compare(password, results[0].password, (err, same) => {
+            if (err) {
+                return res.status(500).send("Authentication server error");
+            }
+            if (same) {
+                // if (result[0].role == 1) {
+                res.send('productslist')
+                // }
+                // if (result[0].role == 2) {
+                //     res.send('productslist')
+                // }
+            } else {
+                res.status(400).send("Wrong password");
+            }
+        });
+    });
+});
+// ----convert password---------
 // app.get('/password/:pass', function (req, res) {
 //     const raw = req.params.pass;
 //     bcrypt.hash(raw, 10, function (err, hash) {
@@ -122,57 +130,44 @@ app.get('/login', function (_req, res) {
 //         }
 //     });
 // });
-app.get('/product', function (req, res) {
-    const sql = "SELECT * FROM `products`";
-    con.query(sql, function (err, results) {
+// app.get('/product', function (req, res) {
+//     const sql = "SELECT * FROM `products`";
+//     con.query(sql, function (err, results) {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send('DB error');
+//         } else {
+
+//         }
+//     });
+// });
+
+// app.get('/productslist', function (_req, res) {
+//     res.sendFile(path.join(__dirname, 'Project/customer/homepage.html'));
+
+// })
+app.get('/cfpage', function (_req, res) {
+    res.sendFile(path.join(__dirname, 'Project/customer/cf_page.html'));
+})
+app.get('/homepage', function (_req, res) {
+    res.sendFile(path.join(__dirname, 'Project/customer/homepage.html'));
+})
+app.get('/product', (req, res) => {
+    const sql = 'SELECT products.*, users.first_name,last_name,profile_img FROM products JOIN users ON products.seller_id = users.users_id WHERE users.role = 2;';
+    con.query(sql, (err, results) => {
         if (err) {
-            console.error(err);
-            res.status(500).send('DB error');
+            res.status(500).json({ error: 'Database query failed' });
         } else {
-            // Map the category to its corresponding icon
-            results.forEach(product => {
-                switch (product.product_cate) {
-                    case 1:
-                        product.category_icon = 'icon-shirt';
-                        break;
-                    case 2:
-                        product.category_icon = 'icon-coat';
-                        break;
-                    case 3:
-                        product.category_icon = 'icon-dress';
-                        break;
-                    case 4:
-                        product.category_icon = 'icon-pants';
-                        break;
-                    case 5:
-                        product.category_icon = 'icon-skirt';
-                        break;
-                    case 6:
-                        product.category_icon = 'icon-sock';
-                        break;
-                    case 7:
-                        product.category_icon = 'icon-shoe';
-                        break;
-                    case 8:
-                        product.category_icon = 'icon-hat';
-                        break;
-                    case 9:
-                        product.category_icon = 'icon-necklace';
-                        break;
-                    case 0:
-                        product.category_icon = 'icon-chair';
-                        break;
-                }
-            });
             res.json(results);
         }
     });
 });
-
-app.get('/productslist', function (_req, res) {
-    res.sendFile(path.join(__dirname, 'Project/customer/homepage.html'));
-
-})
+app.get('/product/:productId', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Project/customer/cf_page.html'))
+});
+app.post('/product/:productId', (req, res) => {
+    const sql = 'SELECT products.*, users.first_name,last_name,profile_img FROM products JOIN users ON products.seller_id = users.users_id WHERE users.role = 2;';
+});
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
